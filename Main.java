50d01c1f458b1ed133ebd1991050c5f7b3e9e105
Akanksha Tomar <akanksha4715@gmail.com>
2026-01-30 10:47:02 +1100

    brute

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] gas = {1,2,3,4,5};
-        int[] cost = {3,4,5,1,2};
+        // int[] gas = {1,2,3,4,5};
+        // int[] cost = {3,4,5,1,2};
+        int[] gas = {3,1,1};
+        int[] cost = {1,2,2};
         System.out.println(sol.canCompleteCircuit(gas, cost));
     }
     
@@ -16,17 +18,18 @@ class Solution {
         for(int i=0; i<n; i++){
             if(gas[i] > cost[i]){
                 start = i;
+                
                 int j = i+1;
                 int g = gas[i] - cost[i];
-                while(j != start){
+                while(j%n != start){
                     j %= n;
                     g = g + gas[j] - cost [j];
-                    if(!(g > 0))
+                    if(!(g >= 0))
                         break;
                     j++;
                     
                 }
-
+                j %= n;
                 if(j == start)
                     return start;
             }
