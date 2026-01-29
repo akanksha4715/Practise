6f794e339b7cfe73a057ca59759aae2530ae7c2c
Akanksha Tomar <akanksha4715@gmail.com>
2026-01-29 19:14:42 +1100

    gasStation

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        System.out.println(sol.canCompleteCircuit(gas, cost));
+    }
+    
+}
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start;
+        int n = gas.length;
+
+        for(int i=0; i<n; i++){
+            if(gas[i] > cost[i]){
+                start = i;
+                int j = i+1;
+                int g = gas[i] - cost[i];
+                while(j != start){
+                    j %= n;
+                    g = g + gas[j] - cost [j];
+                    if(!(g > 0))
+                        break;
+                    j++;
+                    
+                }
+
+                if(j == start)
+                    return start;
+            }
+        }
+
+        return -1;
+    }
+}
