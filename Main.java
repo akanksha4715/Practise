f67a4eda06c78bcc80409b5a44201e90ffebc16e
Akanksha Tomar <akanksha4715@gmail.com>
2026-02-01 13:33:09 +1100

    Candies

--- Main.java
+++ Main.java
@@ -3,35 +3,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        // int[] gas = {1,2,3,4,5};
+        Candies sol = new Candies();
+        int[] ratings = {1,0,2};
         // int[] cost = {3,4,5,1,2};
         int[] gas = {3,1,1};
         int[] cost = {1,2,2};
-        System.out.println(sol.canCompleteCircuit(gas, cost));
+        System.out.println(sol.candy(ratings));
     }
     
-}
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-
-        if(Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) return -1;
-
-        int start=0;
-        int n = gas.length;
-
-        int surplus = 0;
-
-        for(int i=0; i<n; i++){
-            surplus += gas[i] - cost[i];
-
-            if(surplus < 0){
-                start = i+1;
-                surplus = 0;
-            } 
-            
-        }
-
-        return  start ;
-    }
 }
