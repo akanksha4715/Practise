7d914a7fb3ba0831370fd5b5881a5cfd669b7392
Akanksha Tomar <akanksha4715@gmail.com>
2026-03-21 20:39:21 +1100

    initial

---
+++ KthLargestElement.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+
+public class KthLargestElement{
+   public static int findKthLargest(int[] nums, int k) {
+      
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      
+      for(int i=0; i< nums.length; i++){
+         if(pq.size() < k)
+            pq.add(nums[i]);
+         else if(nums[i] > pq.peek()){
+            pq.poll();
+            pq.add(nums[i]);
+         }
+      }
+      
+      return pq.peek();
+      
+      // Replace this placeholder return statement with your code
+      
+   }
+}
