f62b2835d5886f334b1e59cad96eccc34c2315d6
Akanksha Tomar <akanksha4715@gmail.com>
2026-03-21 20:38:02 +1100

    max score

---
+++ MaxScore.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+public class MaxScore {
+    public static int maxScore (int[] nums, int k) {
+      
+        PriorityQueue<Integer> pq = new PriorityQueue<>((p,q) -> q - p);
+        for(int i = 0; i < nums.length; i++){
+            pq.add(nums[i]);
+        }
+        
+        int sum = 0;
+        while( k > 0){
+            int val = pq.poll();
+            sum = sum + val;
+            pq.add((int) Math.ceil(val/3.0));
+            
+            k--;
+        }
+        
+        return sum;
+    }
